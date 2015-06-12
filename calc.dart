b69011a6f87412c4d6a3c51fe74fc968f4fb4556
wooken <kenny@kenwoo.io>
2015-06-11 20:57:53 -0700

    Add calc.dart and corresponding unit test suite

---
+++ calc.dart
@@ -0,0 +1,8 @@
+bool isSetData(String s) {
+	RegExp SetData = new RegExp(r"^([ivx]+\.\s)?[0-9]+x[0-9]+\s@[0-9]+$");
+    return SetData.hasMatch(s);
+}
+
+void main() {
+    print("Hello world!");
+}
---
+++ test/calc_test.dart
@@ -0,0 +1,19 @@
+import "package:test/test.dart";
+import '../calc.dart';  
+
+main() {  
+    group("isSetData()", () {
+        test("rejects names", () {
+            String name = "A. Push Press";
+            expect(isSetData(name), false);
+        });
+        test("accepts data with roman numerals", () {
+            String data = "i. 4x5 @95";
+            expect(isSetData(data), true);
+        });
+        test("accepts data without roman numerals", () {
+            String data = "4x5 @95";
+            expect(isSetData(data), true);
+        });
+    });
+}  
