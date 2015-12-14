eb8e1c1e39c9f71f7215df35aaf3f46f85470bf1
wooken <kenny@kenwoo.io>
2015-12-13 23:44:08 -0800

    Port class: Exer
    This patch contains code that is failing current tests

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var assert = require('chai').assert;
 var Exer = require('../lib/exer');
 var Loggen = require('../lib/loggen');
 
-describe('[Exer]', function () {
+describe('Exer', function () {
   describe('constructor', function () {
     it('sets name', function () {
       var exercise = new Exer('A. Push Press');
@@ -16,13 +16,28 @@ describe('[Exer]', function () {
       assert.equal(exercise.tonnage, 0);
     });
   });
-  describe('addData()', function () {
+  describe('Exer.addData()', function () {
     it('addData() sets data/tonnage', function () {
       var exercise = new Exer("A. Push Press");
       exercise.addData('5x5 @95');
       assert.lengthOf(exercise.data, 1);
       assert.equal(exercise.data[0], '5x5 @95');
-      assert.equal(exercise.tonnage, 2375);
+      assert.equal(exercise.tonnage, 5*5*95);
+    });
+    it("sets multiple data/tonnage", function () {
+      var exercise = new Exer("A. Push Press");
+      exercise.addData("5x5 @95");
+      exercise.addData("5x5 @95");
+      exercise.addData("5x5 @95");
+      assert.lengthOf(exercise.data, 3);
+      assert.equal(exercise.tonnage, 5*5*95*3);
+    });
+    it("ignores whitespace in data", function () {
+      var exercise = new Exer("A. Push Press");
+      exercise.addData("5x5@95");
+      exercise.addData("5 x 5 @ 95");
+      assert.lengthOf(exercise.data, 2);
+      assert.equal(exercise.tonnage, 5*5*95*2);
     });
   });
 });
