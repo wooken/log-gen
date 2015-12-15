172815ec0b874e413532fa86b07497dc5d6e018b
wooken <kenny@kenwoo.io>
2015-12-14 20:53:28 -0800

    Add modules true. Fix js formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,16 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react"
-    ],
-    "env": {
-        "node": true,
-        "mocha": true,
-    },
+  'extends': 'airbnb',
+  'plugins': [
+    'react',
+  ],
+  'env': {
+    'node': true,
+    'mocha': true,
+  },
+  'ecmaFeatures': {
+    'modules': true,
+  },
+  'rules': {
+    'strict': [2, 'global'],
+  },
 };
