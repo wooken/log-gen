69ff2950e32d1796c38cb1ac01b354e3dc6b3bb9
wooken <kenny@kenwoo.io>
2015-12-14 21:16:18 -0800

    Change 'var' for strict mode

--- test/test.js
+++ test/test.js
@@ -1,31 +1,31 @@
 /* eslint-disable func-names */
 
-var assert = require('chai').assert;
-var Exer = require('../lib/exer');
-var Loggen = require('../lib/loggen');
+const assert = require('chai').assert;
+const Exer = require('../lib/exer');
+const Loggen = require('../lib/loggen');
 
 describe('Exer', function () {
   describe('constructor', function () {
     it('sets name', function () {
-      var exercise = new Exer('A. Push Press');
+      const exercise = new Exer('A. Push Press');
       assert.equal(exercise.name, 'A. Push Press');
     });
     it('initializes data/tonnage', function () {
-      var exercise = new Exer('A. Push Press');
+      const exercise = new Exer('A. Push Press');
       assert.lengthOf(exercise.data, 0);
       assert.equal(exercise.tonnage, 0);
     });
   });
   describe('Exer.addData()', function () {
     it('addData() sets data/tonnage', function () {
-      var exercise = new Exer('A. Push Press');
+      const exercise = new Exer('A. Push Press');
       exercise.addData('5x5@95');
       assert.lengthOf(exercise.data, 1);
       assert.equal(exercise.data[0], '5x5@95');
       assert.equal(exercise.tonnage, 5 * 5 * 95);
     });
     it('sets multiple data/tonnage', function () {
-      var exercise = new Exer('A. Push Press');
+      const exercise = new Exer('A. Push Press');
       exercise.addData('5x5@95');
       exercise.addData('5x5@95');
       exercise.addData('5x5@95');
@@ -33,7 +33,7 @@ describe('Exer', function () {
       assert.equal(exercise.tonnage, 5 * 5 * 95 * 3);
     });
     it('ignores whitespace in data', function () {
-      var exercise = new Exer('A. Push Press');
+      const exercise = new Exer('A. Push Press');
       exercise.addData('5x5@95');
       exercise.addData('5x5 @95');
       exercise.addData(' 5 x 5 @ 95 ');
