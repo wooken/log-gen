d3eeaf52ad11bf079c2a0546aabc290fca07416b
wooken <kenny@kenwoo.io>
2016-01-23 17:44:50 -0800

    Handle extra whitespace during parsing

--- test/test.js
+++ test/test.js
@@ -4,16 +4,23 @@ const assert = require('chai').assert
 const src = require('../lib/src')
 
 describe('parseInputStringToMap', function () {
-  it('', function () {
+  it('removes extra whitespace from set data', function () {
+    const result = src.parseInputStringToMap('A1. Squat\n\n\n\n1   x	5 @  225\n\n		2x  3 @255\n   2x2 @285')
+    assert.isTrue('A1. Squat' in result)
+    assert.equal(result['A1. Squat'][0], '1x5@225')
+    assert.equal(result['A1. Squat'][1], '2x3@255')
+    assert.equal(result['A1. Squat'][2], '2x2@285')
+  })
+  it('parses lots of data', function () {
     const result = src.parseInputStringToMap('A1. Squat\n1x5 @225\n2x3 @255\n2x2 @285\nB1. Clean\n1x5 @115\n2x3 @135\n2x2 @145')
     assert.isTrue('A1. Squat' in result)
-    assert.equal(result['A1. Squat'][0], '1x5 @225')
-    assert.equal(result['A1. Squat'][1], '2x3 @255')
-    assert.equal(result['A1. Squat'][2], '2x2 @285')
+    assert.equal(result['A1. Squat'][0], '1x5@225')
+    assert.equal(result['A1. Squat'][1], '2x3@255')
+    assert.equal(result['A1. Squat'][2], '2x2@285')
     assert.isTrue('B1. Clean' in result)
-    assert.equal(result['B1. Clean'][0], '1x5 @115')
-    assert.equal(result['B1. Clean'][1], '2x3 @135')
-    assert.equal(result['B1. Clean'][2], '2x2 @145')
+    assert.equal(result['B1. Clean'][0], '1x5@115')
+    assert.equal(result['B1. Clean'][1], '2x3@135')
+    assert.equal(result['B1. Clean'][2], '2x2@145')
   })
 })
 
