87e9ebee36ad073d640237596d18898aa1a2b3c2
wooken <kenny@kenwoo.io>
2016-03-02 22:40:25 -0800

    Change various things to completion
    - improve exercise name whitespace handling
    - add exercise name whitespace test case
    - add total tonnage tally
    - remove console.log debugging

--- test/test.js
+++ test/test.js
@@ -7,7 +7,6 @@ describe('main', function () {
   it('', function () {
     const userInput = 'A1. Squat\n1x5 @225\n2x3 @255\n2x2 @285\nB1. Clean\n1x5 @115\n2x3 @135\n2x2 @145'
     var output = src.main(userInput)
-    console.log(output)
   })
 })
 
@@ -30,7 +29,7 @@ describe('_parseSetDataToIntArray', function () {
 
 describe('_parseInputStringToMap', function () {
   it('removes extra whitespace from set data', function () {
-    const result = src._parseInputStringToMap('A1. Squat\n\n\n\n1   x	5 @  225\n\n		2x  3 @255\n   2x2 @285')
+    const result = src._parseInputStringToMap('     A1. Squat    \n\n\n\n1   x	5 @  225    \n\n		2x  3 @255\n   2x2 @285')
     const squatArray = [[1, 5, 225], [2, 3, 255], [2, 2, 285]]
     assert.isTrue('A1. Squat' in result)
     assert.isTrue(arraysAreEqual(result['A1. Squat'][0], squatArray[0]))
